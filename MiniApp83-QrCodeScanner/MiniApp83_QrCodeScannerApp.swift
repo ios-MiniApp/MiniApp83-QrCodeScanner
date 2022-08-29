3e52b5c3731db033bf9c735f8e689496ab9e1702
Kota <maedakoutaa@gmail.com>
2022-08-29 10:12:08 +0900

    Initial Commit

---
+++ MiniApp83-QrCodeScanner/MiniApp83_QrCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp83_QrCodeScannerApp.swift
+//  MiniApp83-QrCodeScanner
+//
+//  Created by 前田航汰 on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp83_QrCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
